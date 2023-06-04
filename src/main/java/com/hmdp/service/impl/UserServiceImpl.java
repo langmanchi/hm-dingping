2b2fe8a371b168be62a72d00f44fdfa35d179a89
langmanchi <3475532728@qq.com>
2023-06-05 00:20:58 +0800

    统一登录校验(登录状态判断)“

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
@@ -70,7 +72,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
                 user = createUserWithPhone(phone);
             }
         // 7 保存用户信息到session中   这一步的作用
-            session.setAttribute("user",user);
+            session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
             return Result.ok(); // 不需要返回
     }
 
