dad9cc2034478451a7b1d56059c22e89316b2063
langmanchi <3475532728@qq.com>
2023-06-25 15:42:48 +0800

    添加缓存更新策略缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -17,4 +29,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Override
+    public Result queryById(Long id) {
+        String key =CACHE_SHOP_KEY +id;
+        //1 从redis查询缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //2 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)){//只有为字符串的时候才是true 需添加空值考虑
+            //3 存在，直接返回   需要将string转成对象
+            Shop shop = JSONUtil.toBean(shopJson,Shop.class);
+            return  Result.ok(shop);
+        }
+        // 判断命中的是否是空值  判断是空字符串
+        if (shopJson != null){
+            //返回一个错误信息
+            return Result.fail("店铺信息不存在");
+        }
+
+        //4  不存在，根据id查数据库  查询语句
+        Shop shop = getById(id);
+        //5  不存在，返回错误
+        if (shop == null){
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set(key,"", CACHE_NULL_TTL, TimeUnit.MINUTES);
+
+        }
+        //6  存在，写入redis
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        //7  返回
+        return Result.ok(shop);
+    }
+
+
+
+
+    //缓存的同步更新
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null){
+            return Result.fail("店铺id不能为空");
+        }
+        // 1 更新数据库
+        updateById(shop);
+        // 2 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
+    }
 }
